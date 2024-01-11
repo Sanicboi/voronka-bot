a889f75565161a5efe77eda00197a977d5ab67c5
sanicboi <alexandrzezekalo@gmail.com>
2024-01-11 22:15:28 +0300

    failsafe

--- main.ts
+++ main.ts
@@ -191,6 +191,6 @@ bot.onText(/./, async (msg: Bot.Message) => {
 
 
 } catch (error) {
-    console.log(JSON.stringify(error));
+    console.log(JSON.stringify(error.status));
     throw new Error();
 }
